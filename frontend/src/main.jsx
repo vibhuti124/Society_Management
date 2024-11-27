edb1b9ab4048bba3ed56d0e58815319ddd3dc485
sransh161 <ansh.talaviya@swiftrut.com>
2024-11-28 00:15:29 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {Provider} from "react-redux"
+import store from "./redux/userstore.jsx"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
